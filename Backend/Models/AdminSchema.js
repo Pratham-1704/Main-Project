fd8257e5dcb55bef7616d72282c8d4227a906909
JYOTIRADITYA <jyotupatil2002@gmail.com>
2025-03-17 14:54:38 +0530

    1

--- Backend/Models/AdminSchema.js
+++ Backend/Models/AdminSchema.js
@@ -1,13 +1,34 @@
 const mongoose=require('mongoose');
 
 let AdminSchema=new mongoose.Schema({
-    id: { type: String }, // ID Field
-    name: { type: String },
-    username: { type: String },
-    password: { type: String },
-    mobileno: { type: Number },
-    role: { type: String },
-    status: { type: String },
+    id: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    mobileno: {
+        type: Number,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true
+    }
 });
 
 let Admin=mongoose.model("admin",AdminSchema);
