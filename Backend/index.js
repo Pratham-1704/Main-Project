9a58df13a8504b94d62d3fc0682a27617e39c007
Pratham-1704 <prathamkhape2@gmail.com>
2025-03-17 15:41:19 +0530

    a

--- Backend/index.js
+++ Backend/index.js
@@ -17,13 +17,8 @@ app.get("/",(req, res)=>{
 app.use("/admin",require("./Routes/adminsRoute"));
 app.use("/brand",require("./Routes/brandsRoute"));
 
-<<<<<<< HEAD
 
 const port =8081;
 app.listen([port],()=>{
     console.log("server is running on port http://localhost: "+port );
-=======
-app.listen(8081,()=>{
-    console.log("server is running on port http://localhost:8081");
->>>>>>> 4307f3442e1698577ca5f5ad96b6ff59ac5f518c
 });
