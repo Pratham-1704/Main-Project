757e7039cd4ef7657eca1c6df611f885efe623d5
Prasad-6726 <kumbharprasad6726@gmail.com>
2025-03-17 15:55:39 +0530

    a

--- Backend/Models/StateSchema.js
+++ Backend/Models/StateSchema.js
@@ -8,43 +8,7 @@ const StateSchema = new mongoose.Schema(
             trim: true,
             minlength: [3, "Name must be at least 3 characters long"],
             maxlength: [50, "Name cannot exceed 50 characters"]
-        },
-        username: { 
-            type: String, 
-            required: [true, "Username is required"], 
-            unique: true, 
-            trim: true,
-            minlength: [4, "Username must be at least 4 characters long"],
-            maxlength: [20, "Username cannot exceed 20 characters"]
-        },
-        password: { 
-            type: String, 
-            required: [true, "Password is required"],
-            minlength: [6, "Password must be at least 6 characters long"]
-        }, // Store hashed passwords in production
-        mobileno: { 
-            type: String, 
-            required: [true, "Mobile number is required"], 
-            unique: true, 
-            trim: true, 
-            match: [/^\d{10}$/, "Mobile number must be a valid 10-digit number"]
-        },
-        role: { 
-            type: String, 
-            required: [true, "Role is required"], 
-            enum: {
-                values: ["admin", "superadmin", "user"],
-                message: "Role must be either 'admin', 'superadmin', or 'user'"
-            }
-        },
-        status: { 
-            type: String, 
-            required: [true, "Status is required"], 
-            enum: {
-                values: ["active", "inactive"],
-                message: "Status must be either 'active' or 'inactive'"
-            }
-        }
+        }, 
     }, 
     { timestamps: true }
 );
