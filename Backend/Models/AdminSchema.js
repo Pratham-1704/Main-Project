0083a07908e77cf707b493410e33f54b66f9b5ac
Pratham-1704 <prathamkhape2@gmail.com>
2025-03-17 15:00:36 +0530

    1

--- Backend/Models/AdminSchema.js
+++ Backend/Models/AdminSchema.js
@@ -1,15 +1,37 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-let AdminSchema=new mongoose.Schema({
-    id: { type: String }, // ID Field
-    name: { type: String },
-    username: { type: String },
-    password: { type: String },
-    mobileno: { type: Number },
-    role: { type: String },
-    status: { type: String },
+let schema = new mongoose.Schema({
+    name: { type: String, required: true },
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    district: { type: String, required: true },
+    state: { type: String, required: true },
+    ownername: { type: String, required: true },
+    contact: { 
+        type: Number, 
+        required: true,
+        validate: {
+            validator: function(v) {
+                return /^\d{10}$/.test(v);
+            },
+            message: props => `${props.value} is not a valid 10-digit number!`
+        }
+    },
+    email: { 
+        type: String, 
+        required: true,
+        unique: true,
+        validate: {
+            validator: function(v) {
+                return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v);
+            },
+            message: props => `${props.value} is not a valid email!`
+        }
+    },
+    logopath: { type: String },
+    signaturepath: { type: String },
+    stamppath: { type: String }
 });
 
-let Admin=mongoose.model("admin",AdminSchema);
-
-module.exports=Admin;
+let Agency = mongoose.model("agencies", schema);
+module.exports = Agency;
