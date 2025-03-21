d360b0eeb9383ca793d15eabd0dbcf54c7d44035
Pratham-1704 <prathamkhape2@gmail.com>
2025-03-21 15:05:22 +0530

    A

--- frontend/src/Components/Layout.jsx
+++ frontend/src/Components/Layout.jsx
@@ -5,70 +5,6 @@ import './Layout.css'; // Make sure you include your Layout CSS
 
 const Navbar = () => {
   return (
-<<<<<<< HEAD
-    <div className="layout-wrapper">
-      {/* Navbar */}
-      <div role="navigation" className="navbar-wrapper">
-        <div className="brand-logo">
-          <a href="/">
-            <img src="https://th.bing.com/th/id/OIP.VGH-_2dPONY9FCieQ-r4ZwHaHa?rs=1&pid=ImgDetMain" alt="Logo" />
-          </a>
-        </div><a href=""></a>
-        <div className="brand-phone">
-          <a href="tel:+9002341234">
-            <i className="fa fa-phone me-3"></i> 900 234-1234
-          </a>
-        </div>
-        <div className="text-center">
-          <nav className="top-bar-wrap">
-            <ul className="top-bar">
-              <li className="nav-item">
-                <a href="#" className="nav-link">
-                  <span className="inner-link">Home</span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="#" className="nav-link">
-                  <span className="inner-link">About</span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="#" className="nav-link">
-                  <span className="inner-link">Contact</span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="#" className="nav-link">
-                  <span className="inner-link">Support</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <nav className="main-nav-wrap">
-            <ul className="main-nav">
-              <li className="nav-item">
-                <a href="#" className="nav-link">
-                  <span className="inner-link">Services</span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="#" className="nav-link">
-                  <span className="inner-link">Career</span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="#" className="nav-link">
-                  <span className="inner-link">Features</span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="#" className="nav-link">
-                  <span className="inner-link">Gallery</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
-=======
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       {/* Container wrapper */}
       <div className="container-fluid">
@@ -177,7 +113,6 @@ const Navbar = () => {
               Search
             </button>
           </form>
->>>>>>> 640a0657b4d46e54aee5ba1801b85f197e7dd4e2
         </div>
       </div>
     </nav>
