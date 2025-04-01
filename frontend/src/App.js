ef6a8e0e057bed81b3f145f29070fb331ffd1231
Pratham-1704 <prathamkhape2@gmail.com>
2025-04-01 11:32:45 +0530

    aa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,31 +10,19 @@ import Forgotpassword from './components/Forgotpassword';
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-<<<<<<< HEAD
-        <Route path="/" element={<Login />} />
-        <Route path="/forgot-password" element={<Forgotpassword />} />
-        <Route path="dashboard" element={<Landing />}>
-          <Route path="" element={<Dashboard />} />
-        </Route>
-        <Route path="master" element={<Landing />}>
-          <Route path="clients" element={<Clients />} />
-          <Route path="employees" element={<Employees />} />
-        </Route>
-=======
-        {/* <Route path="/admin-register" element={<AdminRegister />} /> */}
-        <Route path="/admin-login" element={<AdminLogin />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-<<<<<<< HEAD
-        <Route path="/layout" element={<Layout />} />
-
-=======
-        <Route path="/layout" element={< Layout />} />
->>>>>>> 4e01b755a4c8aa43a6644bdad1993da7ae28a041
->>>>>>> 9ba2b018b4de948979f1ef7052f93b3096473d17
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/forgot-password" element={<Forgotpassword />} />
+          <Route path="dashboard" element={<Landing />}>
+            <Route path="" element={<Dashboard />} />
+          </Route>
+          <Route path="master" element={<Landing />}>
+            <Route path="clients" element={<Clients />} />
+            <Route path="employees" element={<Employees />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
