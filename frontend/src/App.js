005f6260d35d5cb0734bd83151e8af6c103bfa00
Prasad-6726 <kumbharprasad6726@gmail.com>
2025-04-03 12:11:40 +0530

    pr

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,13 +10,11 @@ import Categories from './components/user/master/Categories';
 import Admin from './components/user/master/Admin';
 import Products from './components/user/master/Products';
 import Brand from './components/user/master/Brand';
-<<<<<<< HEAD
-import Firms from './components/user/master/Firms';
-import BrandProducts from './components/user/master/BrandProduct';
-import Customers from './components/user/master/Customers';
-=======
->>>>>>> 33c62eed5d38742173d35a4787ca738cae8f49ea
 import FinancialYear from './components/user/master/FinancialYear';
+import Firms from './components/user/master/Firms';
+import BrandProducts from './components/user/master/BrandProducts';
+import Customers from './components/user/master/Customers';
+import { message } from 'antd';
 
 function App() {
   return (
