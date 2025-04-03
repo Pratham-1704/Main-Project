97b71090330b44abb1ad007f2538dd38a46aa67a
Prasad-6726 <kumbharprasad6726@gmail.com>
2025-04-03 14:20:52 +0530

    a

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import Login from './components/Login';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import Landing from './components/user/Landing';
 import Dashboard from './components/user/dashboard/Dashboard';
-//import Clients from './components/user/master/Admin';
 import Forgotpassword from './components/Forgotpassword';
 import Categories from './components/user/master/Categories';
 import Admin from './components/user/master/Admin';
@@ -13,16 +12,9 @@ import Firms from './components/user/master/Firms';
 import BrandProducts from './components/user/master/BrandProduct';
 import Customers from './components/user/master/Customers';
 import FinancialYear from './components/user/master/FinancialYear';
-
 import Lead from './components/user/master/Lead';
 import Quotations from './components/user/master/Quotations';
-
 import Order from './components/user/master/Order';
-
-
-
-
-
 
 function App() {
   return (
@@ -31,13 +23,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/forgot-password" element={<Forgotpassword />} />
-         
+
           <Route path="dashboard" element={<Landing />}>
-       
-            <Route path="" element={<Dashboard />} />
-            
-            
+          <Route path="" element={<Dashboard />} />
           </Route>
+
           <Route path="master" element={<Landing />}>
             <Route path="admin" element={<Admin />} />
             <Route path="categories" element={<Categories />} />
@@ -47,15 +37,12 @@ function App() {
             <Route path="brandproduct" element={<BrandProducts />} />
             <Route path="customers" element={<Customers />} />
             <Route path="financialyear" element={<FinancialYear />} />
-
             <Route path='leads' element={<Lead />} />
-
-            <Route path="Order" element={<Order/>}/>
+            <Route path="Order" element={<Order />} />
             <Route path="quotations" element={<Quotations />} />
             <Route path="leads" element={<Lead />} />
-
-
           </Route>
+          
         </Routes>
       </BrowserRouter>
     </>
